24e376336edbb671c03aac6f3aa34b2180acca1c
unknown <sujanhgs@gmail.com>
2026-06-20 13:17:41 +0530

    CI testing2

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 def test_home():
     response = client.get("/")
     
-    assert response.status_code == 200
+    assert response.status_code == 500
     assert response.json() == {
         "message":"CICD Pipeline Working2"
     }
